47d653480c4c023b847b2a0c80d9854f47aa3c52
luisfbn <luisfbn@hotmail.com>
2020-05-26 23:13:52 -0500

    ESLint

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 //const chalk = require('chalk'); //CommonJs
 import chalk from 'chalk'; //ES6
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
